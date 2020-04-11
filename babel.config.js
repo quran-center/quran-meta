1c72a2bd7779fef6a1a3d9bf05bb6409f98d6053
Huseyn Guliyev <husayt@gmail.com>
2020-04-12 00:33:20 +0100

    add unit-tests

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  //  presets: ["@babel/preset-env"],
+  env: {
+    test: {
+      presets: ["@babel/preset-env"],
+      plugins: [
+        // "@babel/plugin-proposal-class-properties",
+        // "transform-es2015-modules-commonjs",
+        // "babel-plugin-dynamic-import-node"
+      ]
+    }
+  }
+}
