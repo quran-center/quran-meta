7d32b6a9f12e80c9eb62689b99bc13a95f38a410
HG <husayt@gmail.com>
2024-10-17 23:19:42 +0100

    cleanup

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
   test: {
     globals: true,
-    reporters: [
-      ["vitest-sonar-reporter", { outputFile: "sonar-report.xml" }]
-    ],
+
     coverage: {
       include: ["src/**/*.ts"],
       reporter: ["text", "lcov"]
