472366e6506760758a3c3a36966b8971ead37b13
HG <husayt@gmail.com>
2025-02-24 22:12:59 +0000

    feat: better type guards

--- src/typeGuards.ts
+++ src/typeGuards.ts
@@ -2,16 +2,16 @@ import { maxAyahsInSurah, meta } from "./const"
 import { getAyahCountInSurah } from "./getAyahCountInSurah"
 import { AyahId, AyahNo, Juz, Surah, SurahAyah } from "./types"
 
-export function isValidAyahId(x: number): x is AyahId {
-  return Number.isInteger(x) && 1 <= x && x <= meta.numAyahs
+export function isValidAyahId(x: unknown): x is AyahId {
+  return Number.isInteger(x) && 1 <= (x as number) && x as number <= meta.numAyahs
 }
 
-export function isValidAyahNo(x: number): x is AyahNo {
-  return Number.isInteger(x) && 1 <= x && x <= maxAyahsInSurah
+export function isValidAyahNo(x: unknown): x is AyahNo {
+  return Number.isInteger(x) && 1 <= (x as number) && x as number <= maxAyahsInSurah
 }
 
-export function isValidSurah(x: number): x is Surah {
-  return Number.isInteger(x) && 1 <= x && x <= meta.numSurahs
+export function isValidSurah(x: unknown): x is Surah {
+  return Number.isInteger(x) && 1 <= (x as number) && x as number <= meta.numSurahs
 }
 
 /**
@@ -27,13 +27,13 @@ export function isValidSurah(x: number): x is Surah {
  * isValidSurahAyah([115, 1]) // false - there are only 114 surahs
  * ```
  */
-export function isValidSurahAyah(x: [number, number]): x is SurahAyah {
+export function isValidSurahAyah(x: [unknown, unknown]): x is SurahAyah {
   const [surah, ayah] = x
   if (!isValidSurah(surah)) {
     return false
   }
 
-  return (Number.isInteger(ayah) && ayah >= 1 && ayah <= getAyahCountInSurah(surah))
+  return (Number.isInteger(ayah) && (ayah as number) >= 1 && ayah as number <= getAyahCountInSurah(surah))
 }
 
 /**
@@ -41,8 +41,8 @@ export function isValidSurahAyah(x: [number, number]): x is SurahAyah {
  * @param x - The number to check
  * @returns True if the number is an integer between 1 and the total number of Juzs (inclusive)
  */
-export function isValidJuz(x: number): x is Juz {
-  return Number.isInteger(x) && 1 <= x && x <= meta.numJuzs
+export function isValidJuz(x: unknown): x is Juz {
+  return Number.isInteger(x) && 1 <= (x as number) && x as number <= meta.numJuzs
 }
 
 /**
@@ -50,8 +50,8 @@ export function isValidJuz(x: number): x is Juz {
  * @param x - The number to check
  * @returns True if the number is an integer between 1 and the total number of pages (inclusive)
  */
-export function isValidPage(x: number): x is Juz {
-  return Number.isInteger(x) && 1 <= x && x <= meta.numPages
+export function isValidPage(x: unknown): x is Juz {
+  return Number.isInteger(x) && 1 <= (x as number) && x as number <= meta.numPages
 }
 
 /**
