bcc8cc7d5967e61d37fb3b08b2cdee278c850613
HG <husayt@gmail.com>
2025-06-11 16:33:45 +0100

    update examples

--- examples/hello.cjs
+++
@@ -1,5 +0,0 @@
-// node hello.cjs
-
-var quranMeta = require("../dist/index.cjs")
-console.log(" Hello World! ") // => 'Hello World'
-console.log(`There are ${quranMeta.meta.numSurahs} suras in the Holy Quran`) // => '3 red apples'); // => '3 red apples'
