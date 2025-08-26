7e73b69f2bd630f05bf8058f8c8fbb6e903323ce
Abdou <2000abdallah2014@gmail.com>
2025-08-26 22:25:19 +0100

    created getList to import lists dynamically by providing riwayah and list name

--- src/lists/index.ts
+++ src/lists/index.ts
@@ -1,29 +1,7 @@
-import { HafsLists } from "./HafsLists";
-import { QalunLists } from "./QalunLists";
-import { AyahId, SurahInfo } from "../types"
-import { FixedArray } from "../ts-utils"
+import type {Riwayas} from "./types"
 
-type allLists = {
-    HizbEighthList: AyahId[],
-    HizbQuarterList: AyahId[],
-    JuzList: AyahId[],
-    ManzilList: AyahId[],
-    PageList: AyahId[],
-    RukuList: AyahId[],
-    SajdaList: AyahId[],
-    SurahList: FixedArray<SurahInfo, 116>
-}
+const getList = async<R extends keyof Riwayas, L extends keyof Riwayas[R]>(riwaya:R, listName:L): Promise<Riwayas[R]> => {
 
-type missingListsPerRiwaya ={
-    Hafs : "HizbEighthList",
-    Qalun : never
-}
-
-type RiwayaList = {
-    [k in keyof missingListsPerRiwaya] : Omit<allLists,missingListsPerRiwaya[k]>
-}
-
-export const lists : RiwayaList = {
-    Hafs: HafsLists,
-    Qalun: QalunLists
-}
+    return (await import(`./${riwaya}Lists.ts`))[listName]
+};
+getList("Hafs","JuzList").then(console.log)
