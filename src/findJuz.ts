1770399b60ed3bbef1dba2244f53a2c5180e2242
Abdou <2000abdallah2014@gmail.com>
2025-09-06 20:15:40 +0100

    added riwaya argument to Juz functions

--- src/findJuz.ts
+++ src/findJuz.ts
@@ -1,4 +1,5 @@
 import { findAyahIdBySurah } from "./findAyahIdBySurah"
+import { RiwayahsWith } from "./lists/types"
 import { findJuzByAyahId } from "./findJuzByAyahId"
 import { AyahId, AyahNo, Juz, Surah } from "./types"
 
@@ -7,10 +8,11 @@ import { AyahId, AyahNo, Juz, Surah } from "./types"
  *
  * @param surah - The Surah (chapter) number.
  * @param ayah - The Ayah (verse) number. Defaults to 1 if not provided.
+ * @param riwaya - The riwaya. Defaults to "Hafs" if not provided.
  * @returns The Juz (part) number that the given Ayah belongs to.
  */
-export function findJuz(surah: Surah, ayah: AyahNo = 1): Juz {
-  const ayahId: AyahId = findAyahIdBySurah(surah, ayah)
+export function findJuz(surah: Surah, ayah: AyahNo = 1,riwaya:RiwayahsWith<"JuzList">): Juz {
+  const ayahId: AyahId = findAyahIdBySurah(surah, ayah,riwaya)
 
-  return findJuzByAyahId(ayahId)
+  return findJuzByAyahId(ayahId,riwaya)
 }
--- src/findJuzByAyahId.ts
+++ src/findJuzByAyahId.ts
@@ -1,4 +1,5 @@
-import { JuzList } from "./lists/juzList"
+import { getList } from "./lists/index"
+import { RiwayahsWith } from "./lists/types"
 import { AyahId, Juz } from "./types"
 import { binarySearch } from "./utils"
 import { checkValidAyahId } from "./validation"
@@ -7,9 +8,11 @@ import { checkValidAyahId } from "./validation"
  * Finds the Juz (part) of the Quran that contains the given Ayah (verse) ID.
  *
  * @param ayahId - The ID of the Ayah (verse) to find the Juz for.
+ * @param riwaya - The riwaya. Defaults to "Hafs" if not provided.
  * @returns The Juz (part) of the Quran that contains the given Ayah ID.
  */
-export function findJuzByAyahId(ayahId: AyahId): Juz {
+export function findJuzByAyahId(ayahId: AyahId,riwaya:RiwayahsWith<"JuzList">): Juz {
+  const JuzList = getList("JuzList",riwaya)
   checkValidAyahId(ayahId)
 
   // const juz = JuzList.findIndex(x => x > ayahId) - 1
