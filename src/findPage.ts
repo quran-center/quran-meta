85d6066eb046aab8a522aaf8cdd66557163fc67a
Abdou <2000abdallah2014@gmail.com>
2025-09-07 09:36:03 +0100

    added riwaya argument to principal find functions

--- src/findPage.ts
+++ src/findPage.ts
@@ -1,5 +1,6 @@
 import { findAyahIdBySurah } from "./findAyahIdBySurah"
-import { PageList } from "./lists/pageList"
+import { getList } from "./lists/index"
+import { RiwayahsWith } from "./lists/types"
 import { AyahId, AyahNo, Page, Surah } from "./types"
 import { binarySearch } from "./utils"
 import { checkValidSurah } from "./validation"
@@ -8,13 +9,14 @@ import { checkValidSurah } from "./validation"
  * Finds the page number for the given Surah and Ayah number.
  *
  * @param surah - The Surah to find the page for.
+ * @param riwaya - The riwaya. Defaults to "Hafs" if not provided.
  * @param ayah - The Ayah number to find the page for.
  * @returns The page number for the given Surah and Ayah.
  */
-export function findPage(surah: Surah, ayah: AyahNo | AyahId = 1): Page {
+export function findPage(surah: Surah, ayah: AyahNo | AyahId = 1,riwaya?:RiwayahsWith<"PageList">): Page {
   checkValidSurah(surah)
   const ayahId: AyahId = findAyahIdBySurah(surah, ayah as AyahNo)
-
+const PageList = getList("PageList",riwaya)
   // return PageList.findIndex(x => x > ayahId) - 1 as Page
   const jj = binarySearch(PageList, ayahId)
   const page = jj < 0 ? -jj - 2 : jj
--- src/findRubAlHizb.ts
+++ src/findRubAlHizb.ts
@@ -1,22 +1,24 @@
-import { findAyahIdBySurah } from "./findAyahIdBySurah"
-import { findRubAlHizbByAyahId } from "./findRubAlHizbByAyahId"
-import { AyahId, AyahNo, RubAlHizbId, Surah } from "./types"
-import { checkValidSurah } from "./validation"
+import { RiwayahsWithAll } from "./lists/types";
+import { findAyahIdBySurah } from "./findAyahIdBySurah";
+import { findRubAlHizbByAyahId } from "./findRubAlHizbByAyahId";
+import { AyahId, AyahNo, RubAlHizbId, Surah } from "./types";
+import { checkValidSurah } from "./validation";
 
 /**
  * Finds the Juz (part) and Rub-ul-Hizb/Maqra (quarter section) of the Quran that the given Ayah (verse) belongs to.
  *
  * @param surah - The Surah (chapter) number.
  * @param ayah - The Ayah (verse) number. Defaults to 1 if not provided.
-
+ *  @param riwaya - The riwaya. Defaults to "Hafs" if not provided.
  * @returns An object containing the Juz (part) number, Hizb (section) number, and the index of the Hizb that the given Ayah belongs to.
  */
 export function findRubAlHizb(
   surah: Surah,
-  ayah: AyahNo = 1
+  ayah: AyahNo = 1,
+  riwaya?: RiwayahsWithAll<["SurahList", "HizbQuarterList"]>
 ): RubAlHizbId {
-  checkValidSurah(surah)
-  const ayahId: AyahId = findAyahIdBySurah(surah, ayah)
+  checkValidSurah(surah);
+  const ayahId: AyahId = findAyahIdBySurah(surah, ayah, riwaya);
 
-  return findRubAlHizbByAyahId(ayahId)
+  return findRubAlHizbByAyahId(ayahId, riwaya);
 }
