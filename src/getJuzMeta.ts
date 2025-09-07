d8660fad20baf3741c1624426e0d7f56562ba43b
Abdou <2000abdallah2014@gmail.com>
2025-09-07 09:37:48 +0100

    added riwaya argument to meta functions

--- src/getJuzMeta.ts
+++ src/getJuzMeta.ts
@@ -1,28 +1,33 @@
-import { findSurahAyahByAyahId } from "./findSurahAyahByAyahId"
-import { JuzList } from "./lists/juzList"
-import { AyahId, Juz, JuzMeta } from "./types"
-import { checkValidJuz } from "./validation"
+import { findSurahAyahByAyahId } from "./findSurahAyahByAyahId";
+import { getList } from "./lists/index";
+import { RiwayahsWith } from "./lists/types";
+import { AyahId, Juz, JuzMeta } from "./types";
+import { checkValidJuz } from "./validation";
 
 /**
  * Retrieves metadata for a specific Juz of the Quran.
  *
  * @param juzNum - The Juz number to retrieve metadata for (1-30)
- * @returns An object containing the Juz number, first ayah, and last ayah in the Juz
+ * @param riwaya - The riwaya. Defaults to "Hafs" if not provided.
+ *@returns An object containing the Juz number, first ayah, and last ayah in the Juz
  * @throws RangeError If the Juz number is not between 1 and 30
  */
-export function getJuzMeta(juzNum: Juz): JuzMeta {
-  checkValidJuz(juzNum)
-
+export function getJuzMeta(
+  juzNum: Juz,
+  riwaya?: RiwayahsWith<"JuzList">
+): JuzMeta {
+  checkValidJuz(juzNum);
+  const JuzList = getList("JuzList", riwaya);
   const [firstAyahId, nextJuzAyahId]: [AyahId, AyahId] = [
     JuzList[juzNum],
-    JuzList[juzNum + 1]
-  ]
-  const lastAyahId = nextJuzAyahId - 1
+    JuzList[juzNum + 1],
+  ];
+  const lastAyahId = nextJuzAyahId - 1;
   return {
     juzNum,
     firstAyahId,
     lastAyahId,
     first: findSurahAyahByAyahId(firstAyahId),
-    last: findSurahAyahByAyahId(lastAyahId)
-  }
+    last: findSurahAyahByAyahId(lastAyahId),
+  };
 }
--- src/getRukuMeta.ts
+++ src/getRukuMeta.ts
@@ -1,11 +1,13 @@
-import { findSurahAyahByAyahId } from "./findSurahAyahByAyahId"
-import { RukuList } from "./lists/rukuList"
-import { AyahId, RukuMeta } from "./types"
-import { checkValidRuku } from "./validation"
+import { findSurahAyahByAyahId } from "./findSurahAyahByAyahId";
+import { getList } from "./lists/index";
+import { RiwayahsWith } from "./lists/types";
+import { AyahId, RukuMeta } from "./types";
+import { checkValidRuku } from "./validation";
 
 /**
  * Retrieves metadata for a specific Ruku (section) of the Quran.
  * @param rukuNum - The number of the Ruku to retrieve metadata for
+ * @param riwaya - The riwaya. Defaults to "Hafs" if not provided.
  * @returns {@link RukuMeta} An object containing metadata about the Ruku including:
  *  - rukuNum: The Ruku number
  *  - firstAyahId: The global Ayah ID of the first verse in this Ruku
@@ -14,19 +16,22 @@ import { checkValidRuku } from "./validation"
  *  - last: The Surah and Ayah numbers for the last verse
  * @throws Will throw an error if the provided Ruku number is invalid
  */
-export function getRukuMeta(rukuNum: number): RukuMeta {
-  checkValidRuku(rukuNum)
-
+export function getRukuMeta(
+  rukuNum: number,
+  riwaya?: RiwayahsWith<"RukuList">
+): RukuMeta {
+  checkValidRuku(rukuNum);
+  const RukuList = getList("RukuList", riwaya);
   const [firstAyahId, nextRukuAyahId]: [AyahId, AyahId] = [
     RukuList[rukuNum],
-    RukuList[rukuNum + 1]
-  ]
-  const lastAyahId = nextRukuAyahId - 1
+    RukuList[rukuNum + 1],
+  ];
+  const lastAyahId = nextRukuAyahId - 1;
   return {
     rukuNum,
     firstAyahId,
     lastAyahId,
     first: findSurahAyahByAyahId(firstAyahId),
-    last: findSurahAyahByAyahId(lastAyahId)
-  }
+    last: findSurahAyahByAyahId(lastAyahId),
+  };
 }
