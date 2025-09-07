7ea7f52b5cc4c8b4df233899bb730bbc60db2fb1
Abdou <2000abdallah2014@gmail.com>
2025-09-07 09:21:19 +0100

    added riwaya argument to findByAyahId functions

--- src/findRukuByAyahId.ts
+++ src/findRukuByAyahId.ts
@@ -1,12 +1,15 @@
-import { RukuList } from "./lists/rukuList"
-import { AyahId, Ruku } from "./types"
-import { binarySearch } from "./utils"
-import { checkValidAyahId } from "./validation"
+import { getList } from "./lists/index";
+import { RiwayahsWith } from "./lists/types";
+import { AyahId, Ruku } from "./types";
+import { binarySearch } from "./utils";
+import { checkValidAyahId } from "./validation";
 
 /**
  * Finds the Ruku number for a given Ayah ID using binary search.
  *
  * @param ayahId - The unique identifier of an Ayah in format: surah:ayah (e.g., "2:255")
+ * @param riwaya - The riwaya. Defaults to "Hafs" if not provided.
+ *  @param riwaya - The riwaya. Defaults to "Hafs" if not provided.
  * @returns The Ruku number corresponding to the given Ayah ID
  * @throws {@link Error} If the provided Ayah ID is invalid
  *
@@ -16,10 +19,14 @@ import { checkValidAyahId } from "./validation"
  * // Returns the Ruku number containing Ayah 255 of Surah 2
  * ```
  */
-export function findRukuByAyahId(ayahId: AyahId): Ruku {
-  checkValidAyahId(ayahId)
+export function findRukuByAyahId(
+  ayahId: AyahId,
+  riwaya?: RiwayahsWith<"ManzilList">
+): Ruku {
+  checkValidAyahId(ayahId);
+  const RukuList = getList("RukuList", riwaya);
 
-  const jj = binarySearch(RukuList, ayahId)
-  const juz = jj < 0 ? -jj - 2 : jj
-  return juz as Ruku
+  const jj = binarySearch(RukuList, ayahId);
+  const juz = jj < 0 ? -jj - 2 : jj;
+  return juz as Ruku;
 }
--- src/findSurahByAyahId.ts
+++ src/findSurahByAyahId.ts
@@ -1,3 +1,4 @@
+import { RiwayahsWith } from "./lists/types";
 import { findSurahAyahByAyahId } from "./findSurahAyahByAyahId"
 import { AyahId, Surah } from "./types"
 
@@ -5,11 +6,12 @@ import { AyahId, Surah } from "./types"
  * Finds a Surah based on the provided Ayah ID.
  *
  * @param ayaId - The unique identifier of the Ayah
+ *  @param riwaya - The riwaya. Defaults to "Hafs" if not provided.
  * @returns The Surah that contains the specified Ayah
  *
  * @example
  * const surah = findSurahByAyahId(1234);
  */
-export function findSurahByAyahId(ayaId: AyahId): Surah {
-  return findSurahAyahByAyahId(ayaId)[0]
+export function findSurahByAyahId(ayaId: AyahId,riwaya?:RiwayahsWith<"SurahList">): Surah {
+  return findSurahAyahByAyahId(ayaId,riwaya)[0]
 }
