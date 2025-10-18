81f11b3e88ab026547f3d8061150fabd6029f3ff
Abdou <2000abdallah2014@gmail.com>
2025-10-18 15:11:18 +0100

    added riwaya param to findManzil

--- src/findManzil.ts
+++ src/findManzil.ts
@@ -1,15 +1,17 @@
 import { findAyahIdBySurah } from "./findAyahIdBySurah"
 import { findManzilByAyahId } from "./findManzilByAyahId"
 import { AyahId, AyahNo, Manzil, Surah } from "./types"
+import { RiwayahsWithAll } from "./lists/types"
 
 /**
  * Finds the Manzil number for a given Surah and Ayah
  * @param surah - The Surah number or object
  * @param ayah - Optional Ayah number (defaults to 1)
+ * @param riwaya - The riwaya. (defaults to "Hafs")
  * @returns The Manzil number (1-7) containing the specified Ayah
  */
-export function findManzil(surah: Surah, ayah: AyahNo = 1): Manzil {
-  const ayahId: AyahId = findAyahIdBySurah(surah, ayah)
+export function findManzil(surah: Surah, ayah: AyahNo = 1,riwaya?:RiwayahsWithAll<["ManzilList","SurahList"]>): Manzil {
+  const ayahId: AyahId = findAyahIdBySurah(surah, ayah,riwaya)
 
   return findManzilByAyahId(ayahId)
 }
