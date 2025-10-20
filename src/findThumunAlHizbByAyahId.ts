0ec258c2d5b52e07294177e3ccf9e6e18f188804
Abdou <2000abdallah2014@gmail.com>
2025-10-20 13:59:19 +0100

    Add findThumunAlHizb and findThumunAlHizbByAyahId functions with riwaya parameter

---
+++ src/findThumunAlHizbByAyahId.ts
@@ -0,0 +1,24 @@
+import { getList } from "./lists/index";
+import { RiwayahsWith } from "./lists/types";
+import { AyahId, RubAlHizbId } from "./types";
+import { binarySearch } from "./utils";
+import { checkValidAyahId } from "./validation";
+
+/**
+ * Finds the Maqra/Rub-al-Hizb  of the Quran that contains the given Ayah (verse) ID.
+ *
+ * @param ayahId - The ID of the Ayah (verse) to find the Juz for.
+ *  @param riwaya - The riwaya. Defaults to "Qalun" if not provided.
+ * @returns The Maqra of the Quran that contains the given Ayah ID.
+ */
+export function findThumunAlHizbByAyahId(
+  ayahId: AyahId,
+  riwaya: RiwayahsWith<"HizbEighthList"> = "Qalun"
+): RubAlHizbId {
+  checkValidAyahId(ayahId);
+  const HizbEighthList = getList("HizbEighthList", riwaya);
+  // return HizbQuarterList.findIndex(x => x > ayahId) - 1 as RubAlHizbId
+  const jj = binarySearch(HizbEighthList, ayahId);
+  const page = jj < 0 ? -jj - 2 : jj;
+  return page as RubAlHizbId;
+}
