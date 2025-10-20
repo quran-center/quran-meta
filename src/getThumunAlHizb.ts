168a06ff59d08034209d6e1c7a224879e1a93848
Abdou <2000abdallah2014@gmail.com>
2025-10-20 22:09:10 +0100

    Added getThumunAlHizb function

--- src/getThumunAlHizb.ts
+++ src/getThumunAlHizb.ts
@@ -3,22 +3,22 @@ import { HizbId, Juz, ThumunAlHizb, JuzPart, ThumunAlHizbId, RubAlHizbId } from 
 /**
  * Retrieves the basic metadata for a specific Eighth (thumun' al-hizb) of the Quran.
  *
- * @param quarterIndex - The index of the Eighth (thumun' al-hizb) to retrieve metadata for, where 1 is the first quarter.
+ * @param eighthIndex - The index of the Eighth (thumun' al-hizb) to retrieve metadata for, where 1 is the first quarter.
  * @returns An object containing the metadata for the specified quarter, including the juz' (part), hizb (section), and the quarter (rub' al-hizb) index.
  */
 export function getThumunAlHizb(
-  thumunIndex: ThumunAlHizbId,
+  eighthIndex: ThumunAlHizbId,
 ): ThumunAlHizb {
   // return HizbQuarterList[maqra]
 
   // const quarterIndex = HizbQuarterList.findIndex(x => x > ayahId) - 1
 
   // const juz = findJuzByAyahId(ayahId)
-  const juz = (Math.floor((thumunIndex - 1) / 16) + 1) as Juz;
+  const juz = (Math.floor((eighthIndex - 1) / 16) + 1) as Juz;
   // isValidJuz(juz)
   
-  const hizbIndex = (Math.floor((thumunIndex - 1) / 8) + 1) as HizbId;
-  const rubAlHizbId = (Math.floor((thumunIndex - 1) / 2) + 1) as RubAlHizbId;
-  const juzPart = (thumunIndex % 16 || 16) as JuzPart;
-  return { juz, hizbId: hizbIndex, juzPart, rubAlHizbId,thumunAlHizbId: thumunIndex };
+  const hizbIndex = (Math.floor((eighthIndex - 1) / 8) + 1) as HizbId;
+  const rubAlHizbId = (Math.floor((eighthIndex - 1) / 2) + 1) as RubAlHizbId;
+  const juzPart = (eighthIndex % 16 || 16) as JuzPart;
+  return { juz, hizbId: hizbIndex, juzPart, rubAlHizbId,thumunAlHizbId: eighthIndex };
 }
