8b444afdd519158aab562935d0cb9474c1bc3409
HG <husayt@gmail.com>
2025-12-05 18:42:25 +0000

    add oxfmt, remove stylistic

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 import jsLint from "@eslint/js"
-import stylistic from "@stylistic/eslint-plugin"
 import tsDocLint from "eslint-plugin-tsdoc"
 import tsLint from "typescript-eslint"
 
@@ -7,8 +6,7 @@ export default [
   // config parsers
   {
     files: ["src/**.{js,mjs,cjs,ts,mts,jsx,tsx}"],
-    languageOptions: {
-    }
+    languageOptions: {}
   },
   {
     files: ["examples/**.{js,mjs,cjs,ts,mts,jsx,tsx}"],
@@ -28,25 +26,9 @@ export default [
   // syntax rules
   jsLint.configs.recommended,
   ...tsLint.configs.recommended,
-  // code style rules
-  // disable legacy conflict rules about code style
-  stylistic.configs["disable-legacy"],
-  // you can customize or use a preset
-  stylistic.configs.customize({
-    indent: 2,
-    quotes: "double",
-    semi: false,
-    commaDangle: "never"
-  }),
+
   {
-    ignores: [
-      "**/node_modules/",
-      "**/coverage/",
-      "**/dist/",
-      "**/docs/",
-      "**/lib_cjs/",
-      "**/lib_es/"
-    ]
+    ignores: ["**/node_modules/", "**/coverage/", "**/dist/", "**/docs/", "**/lib_cjs/", "**/lib_es/"]
   },
   {
     rules: {
@@ -61,14 +43,13 @@ export default [
   },
   {
     files: ["tests/*.{js,mjs,cjs,ts,mts,jsx,tsx}"],
-    languageOptions: {
-
-    },
+    languageOptions: {},
 
     rules: {
       "@typescript-eslint/no-explicit-any": "off"
     }
-  }, {
+  },
+  {
     plugins: {
       tsdoc: tsDocLint
     },
